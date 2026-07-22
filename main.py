bc467affee0797484abe275be0ba8a98a1113182
C-Soldier <tavarisdujon@gmail.com>
2026-07-21 20:53:59 -0400

    Modified main.py, added 'asteroids' assests and made a few changes in some files

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
+import random
+import time
 
-# Variables for the game window size and images
+# Variables for the game assets
 screen_width = 1280
 screen_height = 720
 background = "Assets/background.png"
 
 player_pos = pygame.Vector2(screen_width / 2, screen_height / 2)
 player_ship = "Assets/player_ship.png"
+
+asteroids = ("Assets/big_asteroid.png", "Assets/asteroid_01.png", "Assets/asteroid_02.png", "Assets/asteroid_03.png", "Assets/asteroid_04.png")
 
 pygame.init()
 
@@ -16,9 +20,22 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+# Functions
+def incoming_asteroids(asteroids, screen):
+    asteroid = random.choice(asteroids)
+    asteroid_size = random.randint(30, 80)
+    
+    asteroid = pygame.transform.scale(pygame.image.load(asteroid).convert_alpha(), (asteroid_size, asteroid_size))
+    
+    time.sleep(2)
+    
+    return screen.blit(asteroid, (random.randint(1, 1279), 0))
+
 # Load background and player ship images
 background = pygame.transform.scale(pygame.image.load(background).convert(), (screen_width, screen_height)) # Load and scale the background image to fit the screen size
 player = pygame.transform.scale(pygame.image.load(player_ship).convert_alpha(), (64, 64))
+
+        
 
 while running:
     # poll for events
@@ -45,6 +62,8 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+
+    incoming_asteroids(asteroids, screen)
     
     # flip() the display to put your work on screen
     pygame.display.flip()
