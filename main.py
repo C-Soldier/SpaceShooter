3772f5979f5cd8455f067433f405821d1a6f7aff
C-Soldier <tavarisdujon@gmail.com>
2026-08-01 20:37:17 -0400

    edited file

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ pygame.init()
 screen_width = 1280
 screen_height = 720
 background = "Assets/background.png"
+sprites = pygame.sprite.Group()
 
 player_pos = pygame.Vector2(screen_width / 2, screen_height / 2)
 player_ship = "Assets/Player/player_ship.png"
-player_projectile = "Assets\Player\player_projectile_1.png"
+player_ship_size = (64, 64)
+
+player_projectile_sprite = "Assets\Player\player_projectile_1.png"
+player_projectile_pos = pygame.Vector2(player_pos.x, player_pos.y)
+player_projectile_size = (8, 32)
+projectile_group = pygame.sprite.Group()
 
 asteroids = ("Assets/Asteroids/big_asteroid.png", "Assets/Asteroids/asteroid_01.png", "Assets/Asteroids/asteroid_02.png", "Assets/Asteroids/asteroid_03.png", "Assets/Asteroids/asteroid_04.png")
-sprites = pygame.sprite.Group()
 asteroid_group = pygame.sprite.Group()
+
 asteroid_event = pygame.USEREVENT + 1
 pygame.time.set_timer(asteroid_event, 1000)
-max_asteroids = 5
+max_asteroids = 10
 
 # Varibles for the game itself
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -28,13 +34,13 @@ dt = 0
 
 # Functions
 class Sprites(pygame.sprite.Sprite):
-    def __init__(self, sprite, x_cord: int=0, y_cord: int=0, size: int=None, speed_x: int=0, speed_y: int=0):
+    def __init__(self, sprite, x_cord: int=0, y_cord: int=0, size: tuple=None, speed_x: int=0, speed_y: int=0):
         super().__init__()
         raw_sprite = pygame.image.load(sprite).convert_alpha()
         if size == None:
             self.image = raw_sprite
         elif size != None:
-            self.image = pygame.transform.scale(raw_sprite, (size, size))
+            self.image = pygame.transform.scale(raw_sprite, size)
         self.rect = self.image.get_rect()
         self.rect.topleft = (x_cord, y_cord)
         
@@ -48,7 +54,7 @@ class Player(Sprites):
     def update(self):
         keys = pygame.key.get_pressed()
 
-        # Player's Movement
+        # Player's Mechanics
         if keys[pygame.K_w]:
             self.rect.y -= self.speed_y * dt
         if keys[pygame.K_s]:
@@ -57,14 +63,23 @@ class Player(Sprites):
             self.rect.x -= self.speed_x * dt
         if keys[pygame.K_d]:
             self.rect.x += self.speed_x * dt
-
+        
         # Keep the player inside the screen bounds
         self.rect.left = max(0, self.rect.left)
         self.rect.right = min(screen_width, self.rect.right)
         self.rect.top = max(0, self.rect.top)
         self.rect.bottom = min(screen_height, self.rect.bottom)
         
+class Projectiles(Sprites):
+    def update(self):
+        mouse = pygame.mouse.get_just_pressed()
         
+        if mouse[0]:
+            self.rect.y -= self.speed_y
+        
+        if self.rect.top < -screen_height:
+            self.kill()
+       
 class Asteroids(Sprites): 
     def update(self):
         self.rect.x += self.speed_x
@@ -77,7 +92,7 @@ class Asteroids(Sprites):
 # Load background and player ship
 background = pygame.transform.scale(pygame.image.load(background).convert(), (screen_width, screen_height)) # Load and scale the background image to fit the screen size
 
-player = Player(player_ship, x_cord=player_pos.x, y_cord=player_pos.y, size=64, speed_x=300, speed_y=300)
+player = Player(player_ship, x_cord=player_pos.x, y_cord=player_pos.y, size=player_ship_size, speed_x=300, speed_y=300)
 sprites.add(player)
 
 while running:
@@ -90,20 +105,24 @@ while running:
         
         if event.type == asteroid_event:
             if len(asteroid_group) < max_asteroids:
-                asteroid = Asteroids(random.choice(asteroids), x_cord=random.randint(0, 1270), y_cord=0, size=asteroid_size, speed_y=random.choices((2, 6), weights=(75, 25), k=1)[0])
+                asteroid = Asteroids(random.choice(asteroids), x_cord=random.randint(0, 1270), y_cord=0, size=(asteroid_size, asteroid_size), speed_y=random.choices((2, 6), weights=(75, 25), k=1)[0])
                 sprites.add(asteroid)
                 asteroid_group.add(asteroid)
-                
-                if pygame.sprite.spritecollide(player, asteroid_group, True):
-                    print("got hit")
-          
+
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
     screen.blit(background)
-    
+
     sprites.update()
+
+    player_projectile = Projectiles(player_projectile_sprite, x_cord=player_projectile_pos.x, y_cord=player_projectile_pos.y, size=player_projectile_size, speed_x=0, speed_y=10)
+    sprites.add(player_projectile)
+    
+    if pygame.sprite.spritecollide(player, asteroid_group, True):
+        print("got hit")
+
     sprites.draw(screen)
     
     # flip() the display to put your work on screen
