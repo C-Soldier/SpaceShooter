a33f65769216bd9ae282cea8b402a603414de90c
C-Soldier <tavarisdujon@gmail.com>
2026-08-08 14:49:08 -0400

    edited files

--- game_functions.py
+++ game_functions.py
@@ -5,6 +5,22 @@ from game_assets import *
 
 class Sprites(pygame.sprite.Sprite):
     def __init__(self, groups: pygame.sprite.Group,
+                 sprite: str,
                  cordinates: pygame.math.Vector2,
-                 size):
+                 speed: int
+                 ):
         super().__init__(groups)
+        self.sprite = sprite
+        self.cordinates = cordinates
+        self.speed = speed
+        self.size = 64
+        
+        self.create_sprite()
+    
+    def create_sprite(self):
+        raw_image = pygame.image.load(self.sprite)
+        if self.size == None:
+            self.image = raw_image.convert_alpha()
+        else:
+            self.image = pygame.transform.scale(raw_image, (self.size, self.size)).convert_alpha()
+        self.rect = self.image.get_rect(center=(self.cordinates))
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 import sys
+from game_functions import Sprites
 from game_constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from game_assets import BACKGROUND
+from game_assets import BACKGROUND, PLAYER_SHIP
 
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 # Load Background
 background = pygame.transform.scale(pygame.image.load(BACKGROUND).convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Load Player
+player_group = pygame.sprite.Group()
+def player():
+    player_sprite_image = PLAYER_SHIP
+    cordinates = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    speed = 300
+    sprite_size = 64
+    Sprites(player_group, player_sprite_image, cordinates, speed)
 
 # Game Loop
 def game_loop():
@@ -17,6 +27,8 @@ def game_loop():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        
+        player()
         
         # Clock
         dt = clock.tick() / 1000
